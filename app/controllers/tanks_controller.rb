9aa998eca8a0ccdc31d3323035c2936967b19285
Suyesh Bhandari <suyeshb@gmail.com>
2016-01-07 12:06:33 -0500

    added tanks

--- app/controllers/tanks_controller.rb
+++ app/controllers/tanks_controller.rb
@@ -1,66 +1,69 @@
 class TanksController < ApplicationController
-  before_action :set_tank, only: [:show, :edit, :update, :destroy]
-  before_action :set_station
+  #before_action :set_tank, only: [:show, :edit, :update, :destroy]
+  #before_action :set_station
+
+  #def set_station
+
+  #end
 
   # GET /tanks
   # GET /tanks.json
   def index
-    @tanks = Tank.all
+    @station = Station.find(params[:station_id])
+    @tanks = @station.tanks
   end
 
   # GET /tanks/1
   # GET /tanks/1.json
   def show
+    @station = Station.find(params[:station_id])
+    @tank = @station.tanks.find(params[:id])
   end
 
   # GET /tanks/new
   def new
-   @task = Tank.new
+   @station = Station.find(params[:station_id])
+   @tank = @station.tanks.build
   end
 
   # GET /tanks/1/edit
   def edit
-
+    @station = Station.find(params[:station_id])
+    @tank = @station.tanks.find(params[:id])
   end
 
   # POST /tanks
   # POST /tanks.json
   def create
-    @tank = Tank.new(tank_params)
-    @tank.station = @station
-    
-    respond_to do |format|
-      if @station.tank.save
-        format.html { redirect_to @tank, notice: 'Tank was successfully created.' }
-        format.json { render :show, status: :created, location: @tank }
-      else
-        format.html { render :new }
-        format.json { render json: @tank.errors, status: :unprocessable_entity }
-      end
+    @station = Station.find(params[:station_id])
+    @tank = @station.tanks.new(tank_params)
+    if @tank.save
+      redirect_to station_tank_path(@station, @tank)
+    else
+      render "new"
     end
   end
 
   # PATCH/PUT /tanks/1
   # PATCH/PUT /tanks/1.json
   def update
-    respond_to do |format|
-      if @station.tank.update(tank_params)
-        format.html { redirect_to @tank, notice: 'Tank was successfully updated.' }
-        format.json { render :show, status: :ok, location: @tank }
-      else
-        format.html { render :edit }
-        format.json { render json: @tank.errors, status: :unprocessable_entity }
-      end
+    @station = Station.find(params[:station_id])
+    @tank = @station.tanks.find(params[:id])
+    if @tank.update(tank_params)
+      redirect_to station_tank_path(@station, @tank)
+    else
+      render "edit"
     end
   end
 
   # DELETE /tanks/1
   # DELETE /tanks/1.json
   def destroy
+    @station = Station.find(params[:station_id])
     @tank = @station.tanks.find(params[:id])
     @tank.destroy
     respond_to do |format|
-      format.html { redirect_to @station, notice: 'Tank was successfully destroyed.' }
+      format.html { redirect_to station_tanks_path(@station), notice: 'Tank was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -68,13 +71,11 @@ class TanksController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
 
-   def set_station
-     @station = Station.find(params[:station_id])
-   end
 
-    def set_tank
-      @tank = Tank.find(params[:id])
-    end
+
+    #def set_tank
+      #@tank = @station.tanks.find(params[:id])
+  #  end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def tank_params
