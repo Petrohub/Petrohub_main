2285f19ec9336ecb9e130fb72390dc986e7b19db
Suyesh Bhandari <suyeshb@gmail.com>
2016-02-01 10:32:53 -0500

    Added retailer id

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160122190503) do
-
-  create_table "retailer_suppliers", force: :cascade do |t|
-    t.integer  "retailer_id"
-    t.integer  "supplier_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_index "retailer_suppliers", ["retailer_id"], name: "index_retailer_suppliers_on_retailer_id"
-  add_index "retailer_suppliers", ["supplier_id"], name: "index_retailer_suppliers_on_supplier_id"
+ActiveRecord::Schema.define(version: 20160119173235) do
 
   create_table "stations", force: :cascade do |t|
     t.string   "brand"
@@ -36,12 +26,13 @@ ActiveRecord::Schema.define(version: 20160122190503) do
     t.string   "state"
     t.string   "zip"
     t.string   "station_reg_number"
+    t.integer  "user_id"
     t.integer  "retailer_id"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
   end
 
-  add_index "stations", ["retailer_id"], name: "index_stations_on_retailer_id"
+  add_index "stations", ["user_id"], name: "index_stations_on_user_id"
 
   create_table "tanks", force: :cascade do |t|
     t.string   "type_of_fuel"
